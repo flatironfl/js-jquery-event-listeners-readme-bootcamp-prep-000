c0f259e4cfa2eb86d3d4f7b999dc1a2f3f766901
First Last <flatironcamp.tc99@mamber.net>
2018-01-31 05:21:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,11 @@
 //define functions here - outside of document.ready; and call them inside
 
 function getIt() {
-  $('p').on('click' , function () {window.alert("Hey!")});
+  $('p').on('click', function () {window.alert("Hey!")});
+}
+
+function frameIt() {
+  $('img .tasty').on('load', function () {$('img .tasty').style('border: red')};
 }
 
 $(document).ready(function(){
