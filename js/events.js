182cecdaae4adbc4b0939377972a82e8dbf223e3
First Last <flatironcamp.tc99@mamber.net>
2018-01-31 05:11:34 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,11 @@
-//define functions here
+//define functions here - outside of document.ready; and call them inside
+
+function getIt() {
+  $('p').on('click' , function () {window.alert("Hey!")});
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
 });
